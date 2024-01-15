408b2b95a76ffa936d126e8fa973729798a819e2
谢贤彬 <xiexianbin@jftech.com>
2024-01-15 20:49:10 +0800

    修复bug

--- lib/src/xb_page.dart
+++ lib/src/xb_page.dart
@@ -142,7 +142,7 @@ abstract class XBPage<T extends XBPageVM> extends XBWidget<T> {
 
   Widget _rootWidget(T vm) {
     return WillPopScope(
-      onWillPop: _onWillPop(vm),
+      onWillPop: vm.onWillPop(),
       child: _buildLoadingContent(vm),
     );
   }
@@ -268,36 +268,4 @@ abstract class XBPage<T extends XBPageVM> extends XBWidget<T> {
   List<Widget>? actions(T vm) {
     return null;
   }
-
-  /// -------------------- function --------------------
-  /// 即将pop
-  _onWillPop(T vm) {
-    if (Platform.isAndroid) {
-      return _androidOnWillPop;
-    } else {
-      if (needIosGestureBack() && _canLoadingPop(vm)) {
-        return null;
-      } else {
-        return _iosOnWillPop;
-      }
-    }
-  }
-
-  /// ios如果用WillPopScope的onWillPop不为null，则返回手势失效，可以用这个控制iOS是否能够返回
-  /// 返回结果无影响，因为只要设置了就不能滑动返回
-  Future<bool> _iosOnWillPop() async {
-    return false;
-  }
-
-  /*
-   * 只有 安卓 的 手势返回 操作，这个方法才生效
-   * 如果需要控制iOS的滑动返回，在NeedIosGestureUtil中配置
-   * */
-  Future<bool> _androidOnWillPop(T vm) async {
-    return onAndroidPhysicalBack() && _canLoadingPop(vm);
-  }
-
-  /// loading是否允许返回
-  bool _canLoadingPop(T vm) =>
-      vm.isLoading == false || needResponseNavigationBarLeftWhileLoading();
 }
--- lib/src/xb_page_vm.dart
+++ lib/src/xb_page_vm.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:xb_scaffold/src/common/xb_fade_widget.dart';
 import 'package:xb_scaffold/xb_scaffold.dart';
@@ -43,4 +45,37 @@ abstract class XBPageVM<T> extends XBVM<T> {
       }
     } catch (e) {}
   }
+
+  /// -------------------- function --------------------
+  /// 即将pop
+  onWillPop() {
+    if (Platform.isAndroid) {
+      return _androidOnWillPop;
+    } else {
+      if ((widget as XBPage).needIosGestureBack() && _canLoadingPop()) {
+        return null;
+      } else {
+        return _iosOnWillPop;
+      }
+    }
+  }
+
+  /// ios如果用WillPopScope的onWillPop不为null，则返回手势失效，可以用这个控制iOS是否能够返回
+  /// 返回结果无影响，因为只要设置了就不能滑动返回
+  Future<bool> _iosOnWillPop() async {
+    return false;
+  }
+
+  /*
+   * 只有 安卓 的 手势返回 操作，这个方法才生效
+   * 如果需要控制iOS的滑动返回，在NeedIosGestureUtil中配置
+   * */
+  Future<bool> _androidOnWillPop() async {
+    return (widget as XBPage).onAndroidPhysicalBack() && _canLoadingPop();
+  }
+
+  /// loading是否允许返回
+  bool _canLoadingPop() =>
+      isLoading == false ||
+      (widget as XBPage).needResponseNavigationBarLeftWhileLoading();
 }
