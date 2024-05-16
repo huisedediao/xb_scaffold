cbab23da679a774f10cda09f799b840ab336ce9c
谢贤彬 <xiexianbin@jftech.com>
2024-05-16 10:05:11 +0800

    pageLog添加时间

--- lib/src/xb_log.dart
+++ lib/src/xb_log.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:xb_scaffold/xb_scaffold.dart';
 
 final List<String> _pageLogList = [];
@@ -7,6 +8,11 @@ List<String> get pageLogList => _pageLogList;
 int get _maxLogLen => maxPageLogLen ?? 30;
 
 void recordPageLog(String msg) {
+  DateTime now = DateTime.now();
+  var formatter = DateFormat('yyyy-MM-dd HH:mm:ss');
+  String formattedTime =
+      formatter.format(now.toUtc().add(const Duration(hours: 8)));
+  msg = "$msg $formattedTime";
   _pageLogList.insert(0, msg);
   if (_pageLogList.length > _maxLogLen) {
     _pageLogList.removeLast();
