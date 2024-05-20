29aee68c145c258398b2454354a6a6f429be3f9d
谢贤彬 <xiexianbin@jftech.com>
2024-05-20 10:33:15 +0800

    - 修改XBParse函数为全局函数

--- lib/src/xb_parse.dart
+++ lib/src/xb_parse.dart
@@ -1,43 +1,41 @@
-class XBParse {
-  // 通用的解析方法，使用泛型T来决定返回值类型
-  static T? parse<T>(dynamic object, [int trueValue = 1]) {
-    if (object == null) return null;
+// 通用的解析方法，使用泛型T来决定返回值类型
+T? xbParse<T>(dynamic object, [int trueValue = 1]) {
+  if (object == null) return null;
 
-    try {
-      if (T == int) {
-        return int.tryParse(object.toString()) as T?;
-      } else if (T == double) {
-        return double.tryParse(object.toString()) as T?;
-      } else if (T == String) {
-        return object.toString() as T?;
-      } else if (T == bool) {
-        if (object is bool) {
-          return object as T?;
-        }
-        final temp = parse<int>(object);
-        return (temp == trueValue) as T?;
+  try {
+    if (T == int) {
+      return int.tryParse(object.toString()) as T?;
+    } else if (T == double) {
+      return double.tryParse(object.toString()) as T?;
+    } else if (T == String) {
+      return object.toString() as T?;
+    } else if (T == bool) {
+      if (object is bool) {
+        return object as T?;
       }
-    } catch (e) {
-      return null;
+      final temp = xbParse<int>(object);
+      return (temp == trueValue) as T?;
     }
+  } catch (e) {
     return null;
   }
+  return null;
+}
 
-  // 通用的解析列表方法，使用泛型T来决定列表元素类型
-  static List<T>? parseList<T>(dynamic object, [int trueValue = 1]) {
-    if (object == null || (object is List) == false) return null;
+// 通用的解析列表方法，使用泛型T来决定列表元素类型
+List<T>? xbParseList<T>(dynamic object, [int trueValue = 1]) {
+  if (object == null || (object is List) == false) return null;
 
-    List<T> ret = [];
-    try {
-      for (var item in object) {
-        T? value = parse<T>(item, trueValue);
-        if (value != null) {
-          ret.add(value);
-        }
+  List<T> ret = [];
+  try {
+    for (var item in object) {
+      T? value = xbParse<T>(item, trueValue);
+      if (value != null) {
+        ret.add(value);
       }
-      return ret;
-    } catch (e) {
-      return null;
     }
+    return ret;
+  } catch (e) {
+    return null;
   }
 }
