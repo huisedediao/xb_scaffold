a75c123fa58ad30e1cb14142ad3791693664c9f8
谢贤彬 <xiexianbin@jftech.com>
2024-05-31 13:55:32 +0800

    XBPreventMultiTask 添加重复回调

--- lib/src/utils/xb_prevent_multi_task.dart
+++ lib/src/utils/xb_prevent_multi_task.dart
@@ -1,13 +1,17 @@
+import 'package:flutter/material.dart';
+
 class XBPreventMultiTask {
   final int intervalMilliseconds;
   XBPreventMultiTask({required this.intervalMilliseconds});
   DateTime _dateTime = DateTime.now();
 
-  execute(void Function() task) {
+  execute(void Function() task, {VoidCallback? onError}) {
     final now = DateTime.now();
     if (now.difference(_dateTime).inMilliseconds > intervalMilliseconds) {
       task();
       _dateTime = now;
+    } else {
+      onError?.call();
     }
   }
 }
