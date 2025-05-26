fbedada3dca0ebeab72f0f475c2c934cffa3d7c8
xiexianbin <>
2025-05-26 11:21:25 +0800

    - bug fixed

--- lib/src/utils/xb_prevent_multi_task.dart
+++ lib/src/utils/xb_prevent_multi_task.dart
@@ -3,11 +3,12 @@ import 'package:flutter/material.dart';
 class XBPreventMultiTask {
   final int intervalMilliseconds;
   XBPreventMultiTask({required this.intervalMilliseconds});
-  DateTime _dateTime = DateTime.now();
+  DateTime? _dateTime;
 
   execute(void Function() task, {VoidCallback? onError}) {
     final now = DateTime.now();
-    if (now.difference(_dateTime).inMilliseconds > intervalMilliseconds) {
+    if (_dateTime == null ||
+        now.difference(_dateTime!).inMilliseconds > intervalMilliseconds) {
       task();
       _dateTime = now;
     } else {
