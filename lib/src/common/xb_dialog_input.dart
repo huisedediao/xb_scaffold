4a71acefd8594068b03b6983e0eb1ea06768a59a
xiexianbin <>
2026-03-10 10:41:37 +0800

    deleted unused import

--- lib/src/common/xb_dialog_input.dart
+++ lib/src/common/xb_dialog_input.dart
@@ -1,5 +1,3 @@
-import 'dart:nativewrappers/_internal/vm/lib/ffi_allocation_patch.dart';
-
 import 'package:flutter/services.dart';
 import 'package:xb_scaffold/xb_scaffold.dart';
 import 'package:flutter/material.dart';
