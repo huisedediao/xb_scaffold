3c0c93355d59e43bcc5d4da3e741b95f688425d6
xiexianbin <>
2026-04-30 17:39:16 +0800

    optimized

--- example/lib/choose_page.dart
+++ example/lib/choose_page.dart
@@ -35,18 +35,17 @@ class ChoosePage extends XBPage<ChoosePageVM> {
   Widget buildPage(BuildContext context) {
     final vm = vmOf(context);
     return ListView.separated(
-      itemCount: vm.titles.length,
+      itemCount: vm.itemCount,
       separatorBuilder: (context, index) {
         return Container(
           height: 10,
         );
       },
       itemBuilder: (context, index) {
-        String title = vm.titles[index];
         return XBCellTitleSubtitle(
           backgroundColor: colors.randColor,
           padding: EdgeInsets.all(spaces.gapDef),
-          title: title,
+          title: vm.titleAt(index),
           onTap: () {
             vm.onTapIndex(index);
           },
@@ -61,56 +60,115 @@ class ChoosePage extends XBPage<ChoosePageVM> {
 class ChoosePageVM extends XBPageVM<ChoosePage> with RouteAware {
   ChoosePageVM({required super.context});
 
-  List<String> titles = [
-    "XBCell demo",
-    "XBCellGroup demo",
-    "XBPage demo",
-    "XBDialogInput demo",
-    "XBButton demo",
-    "XBToast demo",
-    "XBWaitTask demo",
-    "VM访问演示",
-    "TFTest",
-    "Hide top bar test",
-    "Error catch test",
-    "XBRoute 功能测试",
-    "XBImageTestPage",
-    "xb_simple_router 测试页",
-    "xb_network 测试页",
+  final List<_ChooseMenuItem> _menuItems = [
+    const _ChooseMenuItem(
+      title: 'XBCell demo',
+      mode: _ChooseNavMode.get,
+      pageBuilder: _buildXBCellDemo,
+    ),
+    const _ChooseMenuItem(
+      title: 'XBCellGroup demo',
+      pageBuilder: _buildXBCellGroupDemo,
+    ),
+    const _ChooseMenuItem(
+      title: 'XBPage demo',
+      pageBuilder: _buildXBPageDemo,
+    ),
+    const _ChooseMenuItem(
+      title: 'XBDialogInput demo',
+      pageBuilder: _buildXBDialogInputDemo,
+    ),
+    const _ChooseMenuItem(
+      title: 'XBButton demo',
+      pageBuilder: _buildXBButtonDemo,
+    ),
+    const _ChooseMenuItem(
+      title: 'XBToast demo',
+      pageBuilder: _buildXBToastDemo,
+    ),
+    const _ChooseMenuItem(
+      title: 'XBWaitTask demo',
+      pageBuilder: _buildXBWaitTaskDemo,
+    ),
+    const _ChooseMenuItem(
+      title: 'VM访问演示',
+      pageBuilder: _buildVMAccessDemo,
+    ),
+    const _ChooseMenuItem(
+      title: 'TFTest',
+      pageBuilder: _buildTFTest,
+    ),
+    const _ChooseMenuItem(
+      title: 'Hide top bar test',
+      pageBuilder: _buildHideTopBarTest,
+    ),
+    const _ChooseMenuItem(
+      title: 'Error catch test',
+      pageBuilder: _buildErrorTestPage,
+    ),
+    const _ChooseMenuItem(
+      title: 'XBRoute 功能测试',
+      pageBuilder: _buildXBRouteTestPage,
+    ),
+    const _ChooseMenuItem(
+      title: 'XBImageTestPage',
+      pageBuilder: _buildXBImageTestPage,
+    ),
+    const _ChooseMenuItem(
+      title: 'xb_simple_router 测试页',
+      pageBuilder: _buildXBSimpleRouterTestPage,
+    ),
+    const _ChooseMenuItem(
+      title: 'xb_network 测试页',
+      pageBuilder: _buildXBNetworkTestPage,
+    ),
   ];
 
+  int get itemCount => _menuItems.length;
+
+  String titleAt(int index) => _menuItems[index].title;
+
   void onTapIndex(int index) {
-    if (index == 0) {
-      // push(const XBCellDemo());
-      Get.to(() => const XBCellDemo());
-    } else if (index == 1) {
-      push(const XBCellGroupDemo());
-    } else if (index == 2) {
-      push(const XBPageDemo());
-    } else if (index == 3) {
-      push(const XBDialogInputDemo());
-    } else if (index == 4) {
-      push(const XBButtonDemo());
-    } else if (index == 5) {
-      push(const XBToastDemo());
-    } else if (index == 6) {
-      push(const XBWaitTaskDemo());
-    } else if (index == 7) {
-      push(const VMAccessDemo());
-    } else if (index == 8) {
-      push(const TFTest());
-    } else if (index == 9) {
-      push(const HideTopBarTest());
-    } else if (index == 10) {
-      push(const ErrorTestPage());
-    } else if (index == 11) {
-      push(const XBRouteTestPage());
-    } else if (index == 12) {
-      push(const XBImageTestPage());
-    } else if (index == 13) {
-      push(const XBSimpleRouterTestPage());
-    } else if (index == 14) {
-      push(const XBNetworkTestPage());
+    if (index < 0 || index >= _menuItems.length) return;
+    final item = _menuItems[index];
+    if (item.mode == _ChooseNavMode.get) {
+      Get.to(item.pageBuilder);
+    } else {
+      push(item.pageBuilder());
     }
   }
+
+  static Widget _buildXBCellDemo() => const XBCellDemo();
+  static Widget _buildXBCellGroupDemo() => const XBCellGroupDemo();
+  static Widget _buildXBPageDemo() => const XBPageDemo();
+  static Widget _buildXBDialogInputDemo() => const XBDialogInputDemo();
+  static Widget _buildXBButtonDemo() => const XBButtonDemo();
+  static Widget _buildXBToastDemo() => const XBToastDemo();
+  static Widget _buildXBWaitTaskDemo() => const XBWaitTaskDemo();
+  static Widget _buildVMAccessDemo() => const VMAccessDemo();
+  static Widget _buildTFTest() => const TFTest();
+  static Widget _buildHideTopBarTest() => const HideTopBarTest();
+  static Widget _buildErrorTestPage() => const ErrorTestPage();
+  static Widget _buildXBRouteTestPage() => const XBRouteTestPage();
+  static Widget _buildXBImageTestPage() => const XBImageTestPage();
+  static Widget _buildXBSimpleRouterTestPage() =>
+      const XBSimpleRouterTestPage();
+  static Widget _buildXBNetworkTestPage() => const XBNetworkTestPage();
+}
+
+enum _ChooseNavMode {
+  push,
+  get,
+}
+
+class _ChooseMenuItem {
+  final String title;
+  final _ChooseNavMode mode;
+  final Widget Function() pageBuilder;
+
+  const _ChooseMenuItem({
+    required this.title,
+    required this.pageBuilder,
+    this.mode = _ChooseNavMode.push,
+  });
 }
